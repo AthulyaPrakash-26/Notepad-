b4d992cd40ec012a89429e18d6f4b6c39b3ade3b
AthulyaPrakash-26 <55690275+AthulyaPrakash-26@users.noreply.github.com>
2019-10-29 10:22:29 +0530

    Add files via upload

--- NotepadHome.java
+++ NotepadHome.java
@@ -7,17 +7,30 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
 import javax.swing.UIManager;
-import javax.swing.border.EmptyBorder;
+
+import javax.swing.undo.UndoManager;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.KeyStroke;
 import java.awt.event.KeyEvent;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.InputEvent;
+import javax.swing.JEditorPane;
+
+import javax.swing.JFileChooser;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class NotepadHome extends JFrame {
 
-	private JPanel contentPane;
+	private JEditorPane editorPane;
+	private StringBuffer buffer;
 
 	/**
 	 * Launch the application.
@@ -42,74 +55,169 @@ public class NotepadHome extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 1067, 804);
 		this.setTitle("Notepad++");
-		
+
 		JMenuBar menuBar = new JMenuBar();
 		menuBar.setBounds(0, 0, 1051, 29);
 		menuBar.setBackground(UIManager.getColor("CheckBox.background"));
 		setJMenuBar(menuBar);
-				
+
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		getContentPane().add(tabbedPane, BorderLayout.CENTER);
+
+		editorPane = new JEditorPane();
+		editorPane.setBounds(53, 0, 993, 716);
+		// editorScrollPane = new JScrollPane(editorPane);
+		// editorScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+		// editorScrollPane.setPreferredSize(new Dimension(250, 145));
+		// editorScrollPane.setMinimumSize(new Dimension(10, 10));
+
+		JPanel panel = new JPanel();
+		tabbedPane.addTab("New tab", null, panel, null);
+		panel.add(editorPane);
+
 		JMenu mnFile = new JMenu("File");
 		mnFile.setForeground(new Color(0, 0, 0));
 		menuBar.add(mnFile);
-		
+
 		JMenuItem mntmNew = new JMenuItem("New");
 		mntmNew.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, InputEvent.CTRL_MASK));
+		mntmNew.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				JPanel panel1 = new JPanel();
+				tabbedPane.addTab("New tab1", null, panel1, null);
+				JEditorPane editorPane1 = new JEditorPane();
+				editorPane1.setBounds(53, 0, 993, 716);
+				panel1.add(editorPane1);
+				for (int i = 0; i < tabbedPane.getTabCount(); i++) {
+					TabClose tabHeader = new TabClose(tabbedPane, i);
+					tabHeader.apply();
+
+				}
+			}
+		});
 		mnFile.add(mntmNew);
-		
+
 		JMenuItem mntmOpen = new JMenuItem("open");
 		mntmOpen.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, InputEvent.CTRL_MASK));
+		mntmOpen.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ae) {
+
+				JFileChooser fileChooser = new JFileChooser();
+				fileChooser.setCurrentDirectory(new File("."));
+				fileChooser.setAcceptAllFileFilterUsed(false);
+				fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+				int result = fileChooser.showOpenDialog(null);
+				if (result == JFileChooser.APPROVE_OPTION) {
+
+					tabbedPane.addTab("New tab2", null, panel, null);
+					String filePath = fileChooser.getSelectedFile().getPath();
+
+					try {
+						FileInputStream fr = new FileInputStream(filePath);
+						InputStreamReader isr = new InputStreamReader(fr, "UTF-8");
+						BufferedReader reader = new BufferedReader(isr);
+						buffer = new StringBuffer();
+
+						String line = null;
+						while ((line = reader.readLine()) != null) {
+
+							buffer.append(line);
+						}
+
+						reader.close();
+						editorPane.setText(buffer.toString());
+
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+
+			}
+
+		});
+
 		mnFile.add(mntmOpen);
-		
+
 		JMenuItem mntmopenFolder = new JMenuItem("open Containing Folder");
-		mnFile.add( mntmopenFolder);
-		
+		mnFile.add(mntmopenFolder);
+
 		JMenuItem mntmSaveAs = new JMenuItem("Save As");
 		mntmSaveAs.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, InputEvent.CTRL_MASK | InputEvent.ALT_MASK));
 		mnFile.add(mntmSaveAs);
-		
+
 		JMenuItem mntmClose = new JMenuItem("Close");
 		mntmClose.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_W, InputEvent.CTRL_MASK));
 		mnFile.add(mntmClose);
-		
+
 		JMenuItem mntmExit = new JMenuItem("Exit");
 		mntmExit.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F4, InputEvent.ALT_MASK));
+		mntmExit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				System.exit(0);
+			}
+		});
 		mnFile.add(mntmExit);
-		
+
 		JMenu mnEdit = new JMenu("Edit");
 		mnEdit.setForeground(new Color(0, 0, 0));
 		menuBar.add(mnEdit);
-		
+
+		UndoManager undoManager = new UndoManager();
+		// JTextField txtField.getDocument().addUndoableEditListener(undoManager);
+
 		JMenuItem mntmUndo = new JMenuItem("Undo");
+		mntmUndo.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if (undoManager.canUndo()) {
+					undoManager.undo();
+				} else {
+					System.out.println("No Undo Buffer.");
+				}
+			}
+		});
 		mnEdit.add(mntmUndo);
-		
+
 		JMenuItem mntmRedo = new JMenuItem("Redo");
 		mnEdit.add(mntmRedo);
-		
+
 		JMenuItem mntmCut = new JMenuItem("Cut");
+		mntmCut.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				editorPane.cut();
+			}
+		});
+		mntmCut.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_X, InputEvent.CTRL_MASK));
 		mnEdit.add(mntmCut);
-		
+
 		JMenuItem mntmCopy = new JMenuItem("Copy");
+		mntmCopy.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				editorPane.copy();
+			}
+		});
+		mntmCopy.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C, InputEvent.CTRL_MASK));
 		mnEdit.add(mntmCopy);
-		
+
 		JMenuItem mntmPaste = new JMenuItem("Paste");
+		mntmPaste.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				editorPane.paste();
+			}
+		});
+		mntmPaste.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_V, InputEvent.CTRL_MASK));
 		mnEdit.add(mntmPaste);
-		
+
 		JMenuItem mntmDelete = new JMenuItem("Delete");
 		mnEdit.add(mntmDelete);
-		
+
 		JMenu mnSearch = new JMenu("Search");
 		mnEdit.setForeground(new Color(0, 0, 0));
 		menuBar.add(mnSearch);
-		
+
 		JMenuItem mntmFind = new JMenuItem("Find");
 		mnSearch.add(mntmFind);
-		
-		
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
-		
-	
+		panel.setLayout(null);
+
 	}
+
 }
