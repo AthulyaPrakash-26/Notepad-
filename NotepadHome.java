8303b777836f8637c0b0a03cec9acf5e175781d7
AthulyaPrakash-26 <55690275+AthulyaPrakash-26@users.noreply.github.com>
2019-10-31 14:45:44 +0530

    Add files via upload

--- NotepadHome.java
+++ NotepadHome.java
@@ -4,6 +4,7 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.EventQueue;
+import java.awt.Font;
 import java.awt.Toolkit;
 import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.StringSelection;
@@ -11,19 +12,27 @@ import java.awt.datatransfer.StringSelection;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
 import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
 import javax.swing.JTextArea;
 import javax.swing.UIManager;
-
+import javax.swing.undo.CannotUndoException;
 import javax.swing.undo.UndoManager;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.KeyStroke;
+import javax.swing.SwingUtilities;
+
 import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.InputEvent;
+
+import javax.swing.AbstractAction;
+import javax.swing.JDialog;
 import javax.swing.JEditorPane;
 
 import javax.swing.JFileChooser;
@@ -35,11 +44,12 @@ import java.io.InputStreamReader;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
-import javax.swing.event.DocumentEvent;
-import javax.swing.event.DocumentListener;
-import javax.swing.text.Element;
+import javax.swing.event.UndoableEditEvent;
+import javax.swing.event.UndoableEditListener;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.DefaultHighlighter;
+import javax.swing.text.Document;
+import javax.swing.text.Highlighter;
 
 public class NotepadHome extends JFrame {
 
@@ -48,6 +58,8 @@ public class NotepadHome extends JFrame {
 	private static JTextArea textArea;
 	private static JTextArea lines;
 	private static NotepadHome frame;
+	private static JTabbedPane tabbedPane;
+	private static Highlighter.HighlightPainter myHighlightPainter;
 
 	/**
 	 * Launch the application.
@@ -73,17 +85,18 @@ public class NotepadHome extends JFrame {
 		setBounds(100, 100, 1067, 804);
 		this.setTitle("Notepad++");
 
+		myHighlightPainter = new MyHighlightPainter(Color.yellow);
+
 		JMenuBar menuBar = new JMenuBar();
 		menuBar.setBounds(0, 0, 1051, 29);
 		menuBar.setBackground(UIManager.getColor("CheckBox.background"));
 		setJMenuBar(menuBar);
 
-		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane = new JTabbedPane(JTabbedPane.TOP);
 		getContentPane().add(tabbedPane, BorderLayout.CENTER);
 
 		JPanel panel = new JPanel();
-		// tabbedPane.addTab("New tab", null, panel.add(new JEditorPane()), null);
-		tabbedPane.addTab("New tab", null, new JPanel().add(new RowHighlighter()), null);
+		tabbedPane.addTab("New tab 1", null, new JPanel().add(new RowHighlighter()), null);
 		tabbedPane.setSelectedIndex(tabbedPane.getTabCount() - 1);
 
 		JMenu mnFile = new JMenu("File");
@@ -154,8 +167,21 @@ public class NotepadHome extends JFrame {
 
 		mnFile.add(mntmOpen);
 
-		JMenuItem mntmopenFolder = new JMenuItem("open Containing Folder");
+		JMenu mntmopenFolder = new JMenu("open Containing Folder");
 		mnFile.add(mntmopenFolder);
+		JMenuItem explorer = new JMenuItem("Explorer");
+		mntmopenFolder.add(explorer);
+		JMenuItem cmd = new JMenuItem("cmd");
+		cmd.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				try {
+					Runtime.getRuntime().exec(new String[] { "cmd", "/K", "Start" });
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				}
+			}
+		});
+		mntmopenFolder.add(cmd);
 
 		JMenuItem mntmSaveAs = new JMenuItem("Save As");
 		mntmSaveAs.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, InputEvent.CTRL_MASK | InputEvent.ALT_MASK));
@@ -166,8 +192,9 @@ public class NotepadHome extends JFrame {
 		mntmClose.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				int selectedIndex = tabbedPane.getSelectedIndex();
-				System.out.println("Default Index:" + selectedIndex);
-				tabbedPane.remove(selectedIndex);
+				while (selectedIndex > 0) {
+					tabbedPane.remove(selectedIndex);
+				}
 			}
 		});
 		mnFile.add(mntmClose);
@@ -191,67 +218,41 @@ public class NotepadHome extends JFrame {
 			}
 		});
 		mnFile.add(mntmExit);
-		// **************************************************************************
-		JScrollPane scrollpane = new JScrollPane();
-		textArea = new JTextArea();
-		lines = new JTextArea("1");
-
-		lines.setBackground(Color.LIGHT_GRAY);
-		lines.setEditable(false);
-
-		textArea.getDocument().addDocumentListener(new DocumentListener() {
-			public String getText() {
-				System.out.println("testing..");
-				int caretPosition = textArea.getDocument().getLength();
-				Element root = textArea.getDocument().getDefaultRootElement();
-				String text = "1" + System.getProperty("line.separator");
-				for (int i = 2; i < root.getElementIndex(caretPosition) + 2; i++) {
-					text += i + System.getProperty("line.separator");
-				}
-				return text;
-
-			}
-
-			public void changedUpdate(DocumentEvent de) {
-				lines.setText(getText());
-			}
-
-			public void insertUpdate(DocumentEvent de) {
-				lines.setText(getText());
-			}
-
-			public void removeUpdate(DocumentEvent de) {
-				lines.setText(getText());
-			}
-
-		});
-		scrollpane.setViewportView(textArea);
-		scrollpane.setRowHeaderView(lines);
-		scrollpane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-		panel.add(scrollpane);
-
-		// ****************************************************************************************
-
+		
 		JMenu mnEdit = new JMenu("Edit");
 		mnEdit.setForeground(new Color(0, 0, 0));
 		menuBar.add(mnEdit);
 
-		UndoManager undoManager = new UndoManager();
-		// JTextField txtField.getDocument().addUndoableEditListener(undoManager);
-
 		JMenuItem mntmUndo = new JMenuItem("Undo");
+		mntmUndo.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Z, InputEvent.CTRL_MASK));
+		JEditorPane j = (JEditorPane) tabbedPane.getSelectedComponent();
+		j.setFont(new Font("Cambria", 0, 15));
+		final UndoManager editManager = new UndoManager();
+		j.getDocument().addUndoableEditListener(new UndoableEditListener() {
+
+			public void undoableEditHappened(UndoableEditEvent e) {
+				editManager.addEdit(e.getEdit());
+			}
+
+		});
 		mntmUndo.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				if (undoManager.canUndo()) {
-					undoManager.undo();
-				} else {
-					System.out.println("No Undo Buffer.");
+				if (editManager.canUndo()) {
+					editManager.undo();
 				}
 			}
 		});
 		mnEdit.add(mntmUndo);
 
 		JMenuItem mntmRedo = new JMenuItem("Redo");
+		mntmRedo.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Y, InputEvent.CTRL_MASK));
+		mntmRedo.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if (editManager.canRedo()) {
+					editManager.redo();
+				}
+			}
+		});
 		mnEdit.add(mntmRedo);
 
 		JMenuItem mntmCut = new JMenuItem("Cut");
@@ -287,18 +288,107 @@ public class NotepadHome extends JFrame {
 				j.paste();
 			}
 		});
-
 		JMenuItem mntmDelete = new JMenuItem("Delete");
+		mntmDelete.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+
+				JEditorPane j = (JEditorPane) tabbedPane.getSelectedComponent();
+				j.replaceSelection(" ");
+			}
+		});
 		mnEdit.add(mntmDelete);
+		JMenuItem mntmSelect = new JMenuItem("SelectAll");
+		mntmSelect.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_A, InputEvent.CTRL_MASK));
+		mntmSelect.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+
+				JEditorPane j = (JEditorPane) tabbedPane.getSelectedComponent();
+				j.selectAll();
+			}
+		});
+		mnEdit.add(mntmSelect);
 
 		JMenu mnSearch = new JMenu("Search");
 		mnEdit.setForeground(new Color(0, 0, 0));
 		menuBar.add(mnSearch);
 
 		JMenuItem mntmFind = new JMenuItem("Find");
+		mntmFind.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				FindDialog dialog = new FindDialog();
+				dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+				dialog.setVisible(true);
+			}
+		});
 		mnSearch.add(mntmFind);
 		panel.setLayout(null);
 
+		j.addMouseListener(new MouseAdapter() {
+			public void mouseReleased(MouseEvent e) {
+				if (SwingUtilities.isRightMouseButton(e)) {
+					if (editManager.canUndo()) {
+						mntmUndo.setEnabled(true);
+						mntmCopy.setEnabled(true);
+						mntmPaste.setEnabled(true);
+					} else {
+						mntmUndo.setEnabled(false);
+						mntmCopy.setEnabled(false);
+						mntmPaste.setEnabled(false);
+					}
+					if (editManager.canRedo()) {
+						mntmRedo.setEnabled(true);
+					} else {
+						mntmRedo.setEnabled(false);
+					}
+
+					JPopupMenu popup = new JPopupMenu();
+					popup.add(mntmUndo);
+					popup.add(mntmRedo);
+					popup.add(mntmCopy);
+					popup.add(mntmPaste);
+					popup.show(e.getComponent(), e.getX(), e.getY());
+				}
+			}
+		});
+
 	}
 
+	public static void highlight(String searchResult) {
+		JEditorPane editor = (JEditorPane) tabbedPane.getSelectedComponent();
+		removeHighlights(editor);
+
+		try {
+			Highlighter hilite = editor.getHighlighter();
+			Document doc = editor.getDocument();
+			String text = doc.getText(0, doc.getLength());
+			if (!text.toLowerCase().contains(searchResult.toLowerCase())) {
+				FindDialog dialog = new FindDialog();
+			}
+			int pos = 0;
+			while ((pos = text.toUpperCase().indexOf(searchResult.toUpperCase(), pos)) >= 0) {
+				hilite.addHighlight(pos, pos + searchResult.length(), myHighlightPainter);
+				pos += searchResult.length();
+			}
+
+		} catch (BadLocationException e) {
+		}
+	}
+
+	public static void removeHighlights(JEditorPane editor) {
+		Highlighter hilite = editor.getHighlighter();
+		Highlighter.Highlight[] hilites = hilite.getHighlights();
+
+		for (int i = 0; i < hilites.length; i++) {
+			if (hilites[i].getPainter() instanceof MyHighlightPainter) {
+				hilite.removeHighlight(hilites[i]);
+			}
+		}
+	}
+
+	class MyHighlightPainter extends DefaultHighlighter.DefaultHighlightPainter {
+
+		public MyHighlightPainter(Color color) {
+			super(color);
+		}
+	}
 }
