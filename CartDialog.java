976017d4149b576b6b21110979907f50c36bcfcf
AthulyaPrakash-26 <55690275+AthulyaPrakash-26@users.noreply.github.com>
2019-11-06 10:01:07 +0530

    Add files via upload

--- CartDialog.java
+++ CartDialog.java
@@ -2,7 +2,7 @@ package view;
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
-import java.awt.Font;
+import java.util.Arrays;
 import java.util.List;
 
 import javax.swing.JButton;
@@ -13,7 +13,10 @@ import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 import javax.swing.table.DefaultTableModel;
 
+import org.postgresql.util.PSQLException;
+
 import controller.ECommerceViewController;
+import exception.ProductNotFoundException;
 import model.Cart;
 import model.Product;
 
@@ -24,8 +27,6 @@ import javax.swing.JLabel;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JScrollPane;
-import javax.swing.ImageIcon;
-import java.awt.Color;
 
 public class CartDialog extends JDialog {
 
@@ -37,13 +38,14 @@ public class CartDialog extends JDialog {
 	private double total = 0.0;
 	private List<Cart> cartList = null;
 	private Object obj;
+	
 
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
 		try {
-			CartDialog dialog = new CartDialog();
+			CartDialog 	dialog = new CartDialog();
 			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
 			dialog.setVisible(true);
 		} catch (Exception e) {
@@ -55,7 +57,7 @@ public class CartDialog extends JDialog {
 	 * Create the dialog.
 	 */
 	public CartDialog() {
-		setBounds(100, 100, 669, 478);
+		setBounds(100, 100, 698, 478);
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
@@ -64,117 +66,41 @@ public class CartDialog extends JDialog {
 			dtmProduct = new DefaultTableModel(new String[] { "Product Name", "Quantity", "Price", "Total" }, 0);
 			{
 				JScrollPane scrollPane = new JScrollPane();
-				scrollPane.setOpaque(false);
-				scrollPane.getViewport().setOpaque(false);
-				scrollPane.setBounds(41, 64, 558, 259);
+				scrollPane.setBounds(41, 38, 599, 285);
 				contentPanel.add(scrollPane);
 				table = new JTable(dtmProduct);
-				table.setRowHeight(24);
-				table.setFont(new Font("Times New Roman", Font.ITALIC, 15));
-				table.setOpaque(false);
-				 table.getTableHeader().setFont(new Font("Times New Roman", Font.ITALIC, 15));
 				scrollPane.setViewportView(table);
-				table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+				//table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+				//table.setSelectionMode(0);//-------------------------------
 				table.setModel(dtmProduct);
-				table.setRowSelectionAllowed(true);
+				table.selectAll();
+				//table.setRowSelectionAllowed(true);
 //				table.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
 //					public void valueChanged(ListSelectionEvent e) {
-//						obj = dtmProduct.getDataVector().elementAt(table.getSelectedRow());					
-//					}
-//				});
+////						
+////						obj = dtmProduct.getDataVector().elementAt(table.getSelectedRow());					
+//				}
+//			});
 			}
 			try {
 				dtmProduct.setRowCount(0);
 				{
 					textField = new JTextField();
-					textField.setBounds(513, 346, 86, 20);
+					textField.setBounds(554, 346, 86, 20);
 					contentPanel.add(textField);
 					textField.setColumns(10);
 				}
 				{
 					JLabel lblNewLabel = new JLabel("Grand Total :");
-					lblNewLabel.setForeground(Color.GREEN);
-					lblNewLabel.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 15));
-					lblNewLabel.setBounds(411, 348, 92, 14);
+					lblNewLabel.setBounds(435, 349, 72, 14);
 					contentPanel.add(lblNewLabel);
 				}
-				cartList = ECommerceViewController.viewSelectAll();	
-				
-				{
-
-					JPanel buttonPane = new JPanel();
-					buttonPane.setBackground(Color.GREEN);
-					buttonPane.setBounds(436, 378, 163, 35);
-					contentPanel.add(buttonPane);
-					buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
-					{
-						JButton buyButton = new JButton("Buy Now");
-						buyButton.addActionListener(new ActionListener() {
-							public void actionPerformed(ActionEvent arg0) {
-
-								try {
-									obj = dtmProduct.getDataVector().elementAt(table.getSelectedRow());		
-									int row=table.getSelectedRow();
-									String str = obj.toString();
-									System.out.println(str);
-									String[] orderList = str.split(",");
-									String name = orderList [0];
-									String qnty = orderList [1];
-									String price = orderList [2];
-									String total = orderList [3];
-									ECommerceViewController.insertOrder(name, qnty, price, total);
-								//	ECommerceViewController.viewDelete();
-									javax.swing.JOptionPane.showMessageDialog(buyButton, "Order placed", "ECommerce", 2);
-									table.remove(0);
-									dtmProduct.removeRow(table.getSelectedRow());
-								
-								} catch (Exception e) {
-									javax.swing.JOptionPane.showMessageDialog(buyButton, "please select a product", "ECommerce", 2);
-								}
-							}
-						});
-						buyButton.setFont(new Font("Times New Roman", Font.ITALIC, 13));
-						buyButton.setActionCommand("Buy Now");
-						buttonPane.add(buyButton);
-						getRootPane().setDefaultButton(buyButton);
-					}
-					{
-						JButton deleteButton = new JButton("Delete");
-						buttonPane.add(deleteButton);
-						deleteButton.setFont(new Font("Times New Roman", Font.ITALIC, 13));
-						deleteButton.addActionListener(new ActionListener() {
-							public void actionPerformed(ActionEvent arg0) {
-								int numRows = table.getSelectedRows().length;
-								for (int i = 0; i < numRows; i++) {
-									dtmProduct.removeRow(table.getSelectedRow());
-									try {
-										String price = ECommerceViewController.viewReadPrice(cartList.get(i).getProductName());
-										total = total - (Double.parseDouble(price));
-										textField.removeAll();
-										textField.setText(Double.toString(total));
-									} catch (Exception e) {
-										e.printStackTrace();
-									}
-
-								}
-
-							}
-						});
-						deleteButton.setActionCommand("Delete");
-					}
-				}
-				{
-					JLabel lblNewLabel_1 = new JLabel("New label");
-					lblNewLabel_1.setIcon(new ImageIcon("C:\\Users\\1026807\\Desktop\\Images\\home2.jpg"));
-					lblNewLabel_1.setBounds(0, 0, 663, 439);
-					contentPanel.add(lblNewLabel_1);
-				}
-
 				cartList = ECommerceViewController.viewSelectAll();
 
 				if (cartList != null) {
 					for (int i = 0; i < cartList.size(); i++) {
 						String product = ECommerceViewController.viewReadPrice(cartList.get(i).getProductName());
+						//String qty = cartList.get(i).getQuantity();					
 						total = Double.parseDouble(cartList.get(i).getQuantity()) * Double.parseDouble(product) + total;
 						Object row[] = { cartList.get(i).getProductName(), cartList.get(i).getQuantity(), product,
 								Double.parseDouble(cartList.get(i).getQuantity()) * Double.parseDouble(product) };
@@ -187,13 +113,78 @@ public class CartDialog extends JDialog {
 				} else {
 					System.out.println("HI");
 				}
-
-			}
-
-			catch (Exception ex) {
+			} catch (ProductNotFoundException e) {
+			//	javax.swing.JOptionPane.showMessageDialog(dialog, "Product Not found", "ECommerce", 2);
+			} catch (Exception ex) {
 				ex.printStackTrace();
+
 			}
 		}
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			
+				JButton buyButton = new JButton("Buy Now");
+				buyButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent arg0) {
+
+						try {
+							obj = dtmProduct.getDataVector().elementAt(table.getSelectedRow());		
+							//int row=table.getSelectedRow();
+							String str = obj.toString();
+							System.out.println(str);
+							String[] orderList = str.split(",");
+							String name = orderList [0];
+							String qnty = orderList [1];
+							String price = orderList [2];
+							String total = orderList [3];
+							ECommerceViewController.insertOrder(name, qnty, price, total);
+						//	ECommerceViewController.viewDelete();
+							javax.swing.JOptionPane.showMessageDialog(buyButton, "Order placed", "ECommerce", 2);
+							table.remove(0);
+							dtmProduct.removeRow(table.getSelectedRow());
+							dispose();
+						
+						} catch (Exception e) {
+							javax.swing.JOptionPane.showMessageDialog(buyButton, "please select a product", "ECommerce", 2);
+						}
+					}
+				});
+				buyButton.setActionCommand("Buy Now");
+				buttonPane.add(buyButton);
+				getRootPane().setDefaultButton(buyButton);
+			
+			
+				JButton deleteButton = new JButton("Delete");
+				deleteButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent arg0) {
+						int numRows = table.getSelectedRows().length;
+						String value = table.getModel().getValueAt(numRows, 0).toString();
+						for (int i = 0; i < numRows; i++) {
+							dtmProduct.removeRow(table.getSelectedRow());
+							ECommerceViewController.viewDelete(value);
+							try {
+								String price = ECommerceViewController.viewReadPrice(cartList.get(i).getProductName());
+								total = total - (Double.parseDouble(price));
+								textField.removeAll();
+								textField.setText(Double.toString(total));
+							} catch (Exception e) {
+								e.printStackTrace();
+							}
+
+						}
+
+					}
+				});
+				deleteButton.setActionCommand("Delete");
+				buttonPane.add(deleteButton);
+			}
+		
+	}
+
+	protected void updateStock() {
+		
 	}
 
 }
